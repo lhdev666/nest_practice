5a90e2fff9f3b48f95ac454a9ae115c738446b18
liuhai <1079988407@qq.com>
2024-09-05 14:45:58 +0800

    test

--- aop-test/src/app.module.ts
+++ aop-test/src/app.module.ts
@@ -18,6 +18,7 @@ import { LoginGuard } from './login.guard';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
+    
     consumer.apply(LogMiddleware).forRoutes('aaa*');
   }
 }
